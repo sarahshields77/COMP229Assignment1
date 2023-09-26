2ae16d63113360978032def4f769a3b314c7d0e7
sarahshields77 <sshield9@my.centennialcollege.ca>
2023-09-26 10:13:04 -0400

    3rd commit - changes to server.js for deployment to render attempt

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+/*process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var express = require('./config/express');
 var app = express();
 app.listen(5000);
 module.exports = app;
 
-console.log('Server running at http://localhost:5000/');
+console.log('Server running at http://localhost:5000/');*/
+
+// changes below this line from freecodecamp how to deploy nodejs to render
+const express = require('./config/express');
+const app = express();
+const PORT = process.env.PORT || 3030;
+
+// your code
+
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+});
