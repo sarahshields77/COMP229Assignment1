65709e18764b2dad2426c0f0f38cfadee4fb8a45
sarahshields77 <sshield9@my.centennialcollege.ca>
2023-09-26 10:31:36 -0400

    further change to server.js for render deployment

--- server.js
+++ server.js
@@ -7,11 +7,13 @@ module.exports = app;
 console.log('Server running at http://localhost:5000/');*/
 
 // changes below this line from freecodecamp how to deploy nodejs to render
+
 const express = require('./config/express');
 const app = express();
 const PORT = process.env.PORT || 3030;
 
 // your code
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
